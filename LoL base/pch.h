ecd9c75e3959ef3e49f87a8fc18f2f2e94dc63ab
Judo3947 <snowy27282025@outlook.com>
2025-05-01 16:04:01 +0400

    pch.h

---
+++ LoL base/pch.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef PCH_H
+#define PCH_H
+
+#include <iostream>
+#include <Windows.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+#include "include/libmem++/libmem.hpp"
+#include "ImGui/imgui.h"
+#include "ImGui/win32/imgui_impl_win32.h"
+#include "ImGui/dx9/imgui_impl_dx9.h"
+#endif
